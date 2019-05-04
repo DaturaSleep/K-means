381c4fabaa95381f1f4dcad1c56e4bc3dce52b66
Maksym Volkov <maksym.volkov.99@gmail.com>
2019-05-04 13:15:58 +0200

    unnecessary code deleted in Main

--- K-means/src/mpproject4/Main.java
+++ K-means/src/mpproject4/Main.java
@@ -4,9 +4,6 @@ import java.io.IOException;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
-		KMeans k = new KMeans("src/res/iris_all.csv");
-		double[][] distance = { { 5, 3, 8 }, { 1, 4, 5 } };
-		// System.out.println(k.findClassByMin(distance));
-
+		new KMeans("src/res/dane_testowe.csv");
 	}
 }
